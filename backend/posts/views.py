fb8fd38738c9f47cd097e63cb8022f7bd93ddd79
Demeure Romain <demeureromain4@gmail.com>
2022-04-22 14:26:43 +0200

    Ajout authentification api

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from posts.models import BlogPost
 from posts.serializers import BlogPostSerializer
@@ -11,3 +12,4 @@ class BlogPostViewSet(viewsets.ModelViewSet):
 
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    permission_classes = (IsAuthenticated,)
