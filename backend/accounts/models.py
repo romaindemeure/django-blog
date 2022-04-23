a981893065ba52040a6d29d88c1988815a96afc1
Demeure Romain <demeureromain4@gmail.com>
2022-04-23 13:47:56 +0200

    Modification création d'utilisateur

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 # Create your models here.
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None, first_name=None, last_name=None, description=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError("Vous devez entrer un email.")
 
         user = self.model(
-            email.self.normalize_email(email)
+            email=self.normalize_email(email)
         )
 
         user.set_password(password)
         user.save()
-        return self.create_user(email, password, first_name, last_name, description)
+        return user
 
     def create_superuser(self, email, password=None):
         user = self.create_user(email=email, password=password)
@@ -25,17 +25,17 @@ class MyUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser):
-    email = models.EmailField(unique=True, max_length=255, blank=False)
-    first_name = models.CharField(max_length=45, blank=False)
-    last_name = models.CharField(max_length=45, blank=True)
-    description = models.CharField(max_length=144, blank=True)
+    email = models.EmailField(
+        unique=True,
+        max_length=255,
+        blank=False
+    )
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name"]
     object = MyUserManager()
 
     def has_perm(self, perm, obj=None):
